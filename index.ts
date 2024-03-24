d39dbcd3f5334760ccbcaeee06a7a46085086aad
Muhammad Sharjeel Asif <m.sharjeelasif1435@gmail.com>
2024-03-24 23:25:59 +0500

    This is my first calculator

---
+++ index.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter First Number", type: "number", name: "firstNumber" },
+  { message: "Enter Second Number", type: "number", name: "secondNumber" },
+  {
+    message: "Select one of the operator",
+    type: "list",
+    name: "operator",
+    choices: ["Addition", "Subtraction", "Multiplication", "Division", "Exponent"],
+  },
+]);
+
+if (answer.operator === "Addition") {
+  console.log(`Your answer is: ${answer.firstNumber + answer.secondNumber}`);
+} else if (answer.operator === "Subtraction") {
+  console.log(`Your answer is: ${answer.firstNumber - answer.secondNumber}`);
+} else if (answer.operator === "Multiplication") {
+  console.log(`Your answer is: ${answer.firstNumber * answer.secondNumber}`);
+} else if (answer.operator === "Division") {
+  console.log(`Your answer is: ${answer.firstNumber / answer.secondNumber}`);
+} else if (answer.operator === "Exponent") {
+  console.log(`Your answer is: ${answer.firstNumber ** answer.secondNumber}`);
+} else {
+  console.log("Select valid operator");
+}
